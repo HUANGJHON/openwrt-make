2007228470f63ad8b4ac5635cb2ec9e0096cc82f
HUANGJHON <63879186+HUANGJHON@users.noreply.github.com>
2021-06-21 21:55:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,15 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#echo '去除默认bootstrap主题'
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+#echo '修改机器名称'
+#sed -i 's/OpenWrt/PHICOMM K2P/g' package/base-files/files/bin/config_generate
+
+#echo '修改网关地址'
+#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+#echo '修改wifi名称'
+#sed -i 's/OpenWrt/PHICOMM_13/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
